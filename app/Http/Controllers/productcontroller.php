671032ca923e94098e9548bb7f209ac5cb153cfe
GOSWAMI-NAITIK <155132272+GOSWAMI-NAITIK@users.noreply.github.com>
2024-01-03 16:31:25 +0530

    Make add to cart page

--- app/Http/Controllers/productcontroller.php
+++ app/Http/Controllers/productcontroller.php
@@ -4,28 +4,76 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\product;
+use App\Models\cart;
 
 class productcontroller extends Controller
 {
-public function addview(){
+        public function addview($id)
 
 
-return view('addproduct');
-}
-public function add(){
+        {
+                // $product = product::find($id);
+                // DB::enableQueryLog();
+                $product = product::where('id', $id)->first();
 
 
-return view('addproduct');
-}
-public function productview(){
+                $data = product::select('product.*', 'category_master.category_name',)
+                        ->leftJoin('category_master', 'category_master.id', '=', 'product.category_id')->get();
 
-$product = product::all();
- $data = product::select('product.*', 'category_master.category_name',)
-        ->leftJoin('category_master', 'category_master.id', '=', 'product.category_id')->get();
+                $data1 = compact('data', 'product');
 
-        $data1= compact('data','product');
-return view('index')->with($data1);
-}
+                //   dd($data1);
 
 
+
+                return view('addproduct')->with($data1);
+        }
+        public function add(Request $request, $id)
+        {
+                $product = product::find($id);
+
+                $userId = auth()->id();
+                // dd($userId);
+
+
+                $cart = new cart();
+                $cart->product_id = $product->id;
+                $cart->users_id = $userId;
+                $cart->quntity = 1; 
+                $cart->price = $product->product_price; 
+                $cart->product_image_path = $product->product_image_path;  
+                $cart->status = 'pending'; 
+                $cart->save();
+
+                $data111 = compact('cart'); 
+                // dd($cart);
+
+                return redirect()->route('cartview');
+        }
+        // public function add(Request $request ,$id)
+        // {
+        //         $product = product::find($id);
+        //         dd($product);
+
+        //         // return view('addproduct');
+        //         return redirect()->route('cart.add')->with('success', 'Product added to cart successfully.');
+        // }
+        public function productview()
+        {
+
+                $product = product::all();
+                $data = product::select('product.*', 'category_master.category_name',)
+                        ->leftJoin('category_master', 'category_master.id', '=', 'product.category_id')->get();
+
+                $data1 = compact('data', 'product');
+
+                return view('index')->with($data1);
+        }
+
+        public function cartview()
+        {
+
+                
+                
+        }
 }
